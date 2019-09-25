f950b6f60ca7108008dc37d42fce7bdbb8aafa37
mohd12345 <mohd.aazam@susseccive.tech>
2019-09-25 14:22:19 +0530

    setup project structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { LoginPage } from "./app/scenes/login/";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Meeting Room 
-      </header>
+      <LoginPage />
     </div>
   );
 }
---
+++ src/app/scenes/login/loginPage.jsx
@@ -0,0 +1,117 @@
+import React from "react";
+import { withStyles } from "@material-ui/core/styles";
+import {
+  Avatar,
+  Button,
+  CssBaseline,
+  TextField,
+  FormControlLabel,
+  Checkbox,
+  Link,
+  Grid,
+  Typography,
+  Container,
+  InputAdornment,
+  IconButton
+} from "@material-ui/core/";
+import { loginStyles } from "./style";
+import { LockOutlined, VisibilityOff, Visibility } from "@material-ui/icons/";
+
+function SignIn(props) {
+  const { classes } = props;
+
+  const [values, setValues] = React.useState({
+    username: "",
+    password: "",
+    showPassword: false
+  });
+
+  const handleChange = prop => event => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
+
+  const handleClickShowPassword = () => {
+    setValues({ ...values, showPassword: !values.showPassword });
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlined />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <form className={classes.form} noValidate>
+          <TextField
+            fullWidth
+            required
+            onChange={handleChange("username")}
+            value={values.username}
+            variant="outlined"
+            margin="normal"
+            id="username"
+            label="Email Address"
+            name="username"
+            autoComplete="email"
+          />
+          <TextField
+            required
+            fullWidth
+            type={values.showPassword ? "text" : "password"}
+            onChange={handleChange("password")}
+            value={values.password}
+            variant="outlined"
+            margin="normal"
+            name="password"
+            label="Password"
+            id="password"
+            autoComplete="current-password"
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton
+                    edge="end"
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                  >
+                    {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              )
+            }}
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href="#" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+    </Container>
+  );
+}
+
+export default withStyles(loginStyles)(SignIn);
